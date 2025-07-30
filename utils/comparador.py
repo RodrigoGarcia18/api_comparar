8d56809ce79d7fe9a3125a7d82738806b0a7dbd2
RodrigoGarcia18 <rodri0259@hotmail.com>
2025-07-30 18:07:16 -0500

    prueba

--- utils/comparador.py
+++ utils/comparador.py
@@ -1,4 +1,5 @@
-import cv2, numpy as np
+import cv2
+import numpy as np
 from skimage.metrics import structural_similarity as ssim
 from PIL import Image
 import imagehash
@@ -7,15 +8,19 @@ import face_recognition
 def comparar_imagenes(img1_path, img2_path):
     detalles = {}
 
-    # Leer imágenes
+    # Leer imágenes con OpenCV
     img1 = cv2.imread(img1_path)
     img2 = cv2.imread(img2_path)
+
+    if img1 is None or img2 is None:
+        raise ValueError("Error leyendo imágenes")
+
     img2 = cv2.resize(img2, (img1.shape[1], img1.shape[0]))
 
     # SSIM
     gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-    score, diff = ssim(gray1, gray2, full=True)
+    score, _ = ssim(gray1, gray2, full=True)
     detalles["SSIM"] = round(score * 100, 2)
 
     # Diferencia de píxeles
@@ -30,16 +35,20 @@ def comparar_imagenes(img1_path, img2_path):
     distancia_hash = hash1 - hash2
     detalles["ImageHash"] = round(max(0, (1 - distancia_hash / 64) * 100), 2)
 
-    # Face Recognition (si hay rostros)
+    # Face Recognition
     try:
-        encodings1 = face_recognition.face_encodings(face_recognition.load_image_file(img1_path))[0]
-        encodings2 = face_recognition.face_encodings(face_recognition.load_image_file(img2_path))[0]
-        dist = face_recognition.face_distance([encodings1], encodings2)[0]
-        detalles["FaceRecognition"] = round((1 - dist) * 100, 2)
-    except:
+        enc1 = face_recognition.face_encodings(face_recognition.load_image_file(img1_path))
+        enc2 = face_recognition.face_encodings(face_recognition.load_image_file(img2_path))
+
+        if enc1 and enc2:
+            dist = face_recognition.face_distance([enc1[0]], enc2[0])[0]
+            detalles["FaceRecognition"] = round((1 - dist) * 100, 2)
+        else:
+            detalles["FaceRecognition"] = 0.0
+    except Exception:
         detalles["FaceRecognition"] = 0.0
 
-    # Porcentaje final (ponderado)
+    # Promedio final
     similitud_total = round(np.mean(list(detalles.values())), 2)
 
     return {
